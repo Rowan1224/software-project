0959ad60fb42f07fbee643893c2a0555d165ffe2
Silviya Silwal <32938107+ssilwalcode@users.noreply.github.com>
2023-02-05 19:35:50 +0100

    minor bug fix

--- setup.sh
+++ setup.sh
@@ -4,3 +4,4 @@ pip install -r requirements.txt
 
 gdown --fuzzy 'https://drive.google.com/file/d/1oNs3dNAq0r0yUInF9oppiJF7kp0pKt63/view?usp=sharing'
 unzip cleaned_files_civile.zip -d ./dataset
+rm cleaned_files_civile.zip
